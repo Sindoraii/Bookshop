9c6e59ffb39b04a58ba41cb12ffa5fa216a2bd6f
Mariya Krupenina <marise.sindorai@yandex.ru>
2022-09-22 17:55:47 +0300

    refactor: add if statement instead of switch statement

--- components/bookCard/BookCard.js
+++ components/bookCard/BookCard.js
@@ -27,100 +27,100 @@
         card.appendChild(author);
         card.appendChild(price);
 
-        switch (type) {
-            case 'long':
-                const description = document.createElement('a');
-                description.innerHTML = 'Show more';
+        if (type === 'long') {
+            const description = document.createElement('a');
+            description.innerHTML = 'Show more';
 
-                const addToBasketButton  = document.createElement('button');
-                addToBasketButton.innerHTML = 'ADD TO BASKET';
+            const addToBasketButton = document.createElement('button');
+            addToBasketButton.innerHTML = 'ADD TO BASKET';
 
-                const popup = new DescPopup(this.book.description);
-                const body = document.getElementsByTagName('body')[0];
+            const popup = new DescPopup(this.book.description);
+            const body = document.getElementsByTagName('body')[0];
 
-                card.classList.add("book-card");
-                bookImg.classList.add('book-card__img');
-                description.classList.add('book-card__description');
-                addToBasketButton.classList.add('button','book-card__button');
+            card.classList.add("book-card");
+            bookImg.classList.add('book-card__img');
+            description.classList.add('book-card__description');
+            addToBasketButton.classList.add('button', 'book-card__button');
 
-                description.addEventListener('click',()=> {
-                  popup.mount(body);
-                });
+            description.addEventListener('click', () => {
+                popup.mount(body);
+            });
 
-                /* event */
-                addToBasketButton.addEventListener('click',()=>{
-                    basket.updateBasketList(this.book);
-                });
+            /* event */
+            addToBasketButton.addEventListener('click', () => {
+                basket.updateBasketList(this.book);
+            });
 
-                card.appendChild(description);
-                card.appendChild(addToBasketButton);
+            card.appendChild(description);
+            card.appendChild(addToBasketButton);
 
-                break;
-            case 'small':
-                const wrapper = document.createElement('div');
-                wrapper.classList.add('basket__wrapper-qty');
+        } else if (type === 'small') {
+            const wrapper = document.createElement('div');
+            wrapper.classList.add('basket__wrapper-qty');
 
-                const increaseButton =  document.createElement('button');
-                increaseButton.setAttribute('type','button');
-                increaseButton.classList.add('basket__increase-button');
+            const increaseButton = document.createElement('button');
+            increaseButton.setAttribute('type', 'button');
+            increaseButton.classList.add('basket__increase-button');
 
-                const counter = document.createElement('p');
-                counter.classList.add('basket__counter');
-                counter.innerHTML = '1';
-                let counterNumber = Number(counter.innerHTML);
+            const counter = document.createElement('p');
+            counter.classList.add('basket__counter');
+            counter.innerHTML = '1';
+            let counterNumber = Number(counter.innerHTML);
 
-                const decreaseButton =  document.createElement('button');
-                decreaseButton.setAttribute('type','button');
-                decreaseButton.classList.add('basket__decrease-button');
+            const decreaseButton = document.createElement('button');
+            decreaseButton.setAttribute('type', 'button');
+            decreaseButton.classList.add('basket__decrease-button');
 
-                const equelElem  = document.createElement('p');
-                equelElem.innerHTML = '=';
+            const equelElem = document.createElement('p');
+            equelElem.innerHTML = '=';
 
-                const sum = document.createElement('p');
-                sum.classList.add('basket__sum');
-                sum.innerHTML = this.book.price + '$';
+            const sum = document.createElement('p');
+            sum.classList.add('basket__sum');
+            sum.innerHTML = this.book.price + '$';
 
-                const closeButton = document.createElement('button');
-                closeButton.setAttribute('type','button');
-                closeButton.classList.add('close-button','basket__close-button');
+            const closeButton = document.createElement('button');
+            closeButton.setAttribute('type', 'button');
+            closeButton.classList.add('close-button', 'basket__close-button');
 
-                card.classList.add( "book-card", "book-card_small",'basket__card');
-                bookImg.classList.add('book-card__img','book-card__img_small');
+            card.classList.add("book-card", "book-card_small", 'basket__card');
+            bookImg.classList.add('book-card__img', 'book-card__img_small');
 
-                /* events */
-                closeButton.addEventListener('click', () => {
-                    let card = closeButton.parentNode;
-                    let sumNode = card.querySelector('.basket__sum');
-                    let sum = parseInt(sumNode.innerHTML);
-                    basket.decreaseTotal(sum);
-                    card.remove();
-                })
+            /* events */
+            closeButton.addEventListener('click', () => {
+                let card = closeButton.parentNode;
+                let sumNode = card.querySelector('.basket__sum');
+                let sum = parseInt(sumNode.innerHTML);
+                basket.decreaseTotal(sum);
+                card.remove();
+            })
 
-                increaseButton.addEventListener('click',()=>{
-                    counterNumber++;
+            increaseButton.addEventListener('click', () => {
+                counterNumber++;
+                counter.innerHTML = String(counterNumber);
+                sum.innerHTML = String(this.book.price * counterNumber) + '$';
+                basket.increaseTotal(this.book.price);
+
+            })
+            decreaseButton.addEventListener('click', () => {
+                if (counterNumber > 1) {
+                    let sumNumber = parseInt(sum.innerHTML);
+                    counterNumber--;
                     counter.innerHTML = String(counterNumber);
-                    sum.innerHTML = String(this.book.price * counterNumber) + '$';
-                    basket.increaseTotal(this.book.price);
+                    sum.innerHTML = String(sumNumber - this.book.price) + '$';
+                    basket.decreaseTotal(this.book.price);
+                }
+            })
 
-                })
-                decreaseButton.addEventListener('click',()=>{
-                    if(counterNumber > 1) {
-                        let sumNumber = parseInt(sum.innerHTML);
-                        counterNumber--;
-                        counter.innerHTML = String(counterNumber);
-                        sum.innerHTML = String( sumNumber - this.book.price) + '$';
-                        basket.decreaseTotal(this.book.price);
-                    }
-                })
-
-                wrapper.appendChild(increaseButton);
-                wrapper.appendChild(counter);
-                wrapper.appendChild(decreaseButton);
-                wrapper.appendChild(equelElem);
-                wrapper.appendChild(sum);
-                card.appendChild(wrapper);
-                card.appendChild(closeButton);
+            wrapper.appendChild(increaseButton);
+            wrapper.appendChild(counter);
+            wrapper.appendChild(decreaseButton);
+            wrapper.appendChild(equelElem);
+            wrapper.appendChild(sum);
+            card.appendChild(wrapper);
+            card.appendChild(closeButton);
         }
+
+
         /* methods */
         this.mount = (parent) => {
             if(parent instanceof HTMLElement) {
